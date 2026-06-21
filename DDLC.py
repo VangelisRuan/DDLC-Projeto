4674b2d01b730886cc3417f7d9d4fd11f3ec7dd8
VangelisRuan <vangelis.projects@hotmail.com>
2026-06-21 06:37:20 -0300

    Jogo_Teste

--- DDLC.py
+++ DDLC.py
@@ -1,11 +1,16 @@
 import pygame
 pygame.init()
 
-#Posição e velocidade do objeto inicio
-x = 450
-y = 100
+#Posição e velocidade dos objetos inicio
+x = 150
+y = 0
+caixa_x = 0
+caixa_y = 0
+posicao_x = 0
+posicao_y = 0
 velocidade = 5
-#Posição e velocidade do objeto fim
+velocidade_Do_Andar = 50
+#Posição e velocidade dos objetos fim
 
 #Programa para criar o fundo inicio
 fundo = pygame.image.load('ufrpe.jpeg')
@@ -19,8 +24,19 @@ repete_Musica = True
 #Programa para criar a música fim
 
 #Programa para criar o personagem inicio
-personagem_Monika = pygame.image.load('Monika_Teste.png')
+personagem_Monika = pygame.image.load('Monika_Teste_2.png')
+personagem_Monika = pygame.transform.scale(personagem_Monika, (600,600))
 #Programa para criar o personagem fim
+
+personagem_Yuri = pygame.image.load('Yuri_Teste.png')
+personagem_Yuri = pygame.transform.scale(personagem_Yuri, (600,600))
+personagem_Sayori = pygame.image.load('Sayori_Teste.png')
+personagem_Sayori = pygame.transform.scale(personagem_Sayori, (600,600))
+personagem_Natsuki = pygame.image.load('Natsuki_Teste.png')
+personagem_Natsuki = pygame.transform.scale(personagem_Natsuki, (600,600))
+
+caixaDeDialogo = pygame.image.load('caixa_De_Dialogo.png')
+caixaDeDialogo = pygame.transform.scale(caixaDeDialogo, (1200,600))
 
 #Programa para criar e fechar uma janela inicio
 
@@ -29,7 +45,9 @@ pygame.display.set_caption("Projeto Doki Doki Literature Club UFRPE Python")
 
 janela_aberta = True
 while janela_aberta:
-    pygame.time.delay(50)
+#Comando para limitar o número de frames por segundo inicio
+    #pygame.time.delay(50)
+#Comando para limitar o número de frames por segundo fim
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -51,6 +69,15 @@ while janela_aberta:
             #Fora do loop principal
 #Programa para executar comandos do mouse fim
 
+#Programa para personagem sumir quando chegar no final da tela inicio
+    if (posicao_x >= 1500):
+        posicao_x = 0
+#Programa para personagem sumir quando chegar no final da tela fim
+
+#Comando para o personagem andar inicio 
+    #posicao_x += velocidade_Do_Andar
+#Comando para o personagem andar fim
+
 #Colacar e atualizar a janela de fundo para não ficar desenhando durante a atualização do objeto inicio
     #janela.fill((0,0,0))
     janela.blit(fundo, (0,0))
@@ -60,6 +87,10 @@ while janela_aberta:
     #pygame.draw.circle(janela, (0,255,0), (x,y), 50)
 #Aparecendo a Monika inicio
     janela.blit(personagem_Monika, (x,y))
+    janela.blit(personagem_Yuri, (posicao_x + 425,posicao_y))
+    janela.blit(personagem_Sayori, (posicao_x + 675,posicao_y))
+    janela.blit(personagem_Natsuki, (posicao_x - 125,posicao_y))
+    janela.blit(caixaDeDialogo, (caixa_x,caixa_y))
     pygame.display.update()
 #Aparecendo a Monika fim
 #Programa de desenho fim
